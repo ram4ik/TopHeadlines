50fb9129316c96cf35417b658447696d223c2769
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-08-09 13:52:55 +0300

    Initial Commit

---
+++ TopHeadlines/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TopHeadlines
+//
+//  Created by ramil on 09.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TopHeadlines/TopHeadlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopHeadlinesApp.swift
+//  TopHeadlines
+//
+//  Created by ramil on 09.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TopHeadlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
